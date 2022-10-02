beaeab93671b22f9ffdfe5c41697aff4aa78d34b
Matt Hambro <matthew.hambro@gmail.com>
2022-10-01 17:27:27 -0700

    BFS with path to item

--- algorithms/breadth_first_search.go
+++ algorithms/breadth_first_search.go
@@ -13,21 +13,19 @@ Space Complexity:
 
 */
 
-func BreadthFirstSearch(start int, end int, graph map[int][]int) bool {
+func BreadthFirstSearchBool(start int, end int, graph map[int][]int) bool {
 	queue := make([]int, 0)
-	queue = append(queue, graph[start]...)
+	queue = append(queue, start)
+
 	visited := make(map[int]bool)
-	found := false
+
 	for len(queue) > 0 {
-
 		node := queue[0]
-
 		queue = queue[1:]
 
 		visited[node] = true
 		if end == node {
-			found = true
-			break
+			return true
 		}
 
 		for _, v := range graph[node] {
@@ -36,5 +34,44 @@ func BreadthFirstSearch(start int, end int, graph map[int][]int) bool {
 			}
 		}
 	}
-	return found
+	return false
+}
+
+func BreadthFirstSearchPath(start int, end int, graph map[int][]int) []int {
+	queue := make([]int, 0)
+	queue = append(queue, start)
+
+	visited := make(map[int]bool)
+	parents := make(map[int]int)
+
+	found := false
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		visited[node] = true
+
+		if end == node {
+			found = true
+			break
+		}
+
+		for _, v := range graph[node] {
+			if _, ok := visited[v]; !ok {
+				parents[v] = node
+				queue = append(queue, v)
+			}
+		}
+	}
+
+	if found {
+		path := []int{end}
+		for node, ok := parents[end]; ok; node, ok = parents[node] {
+			path = append([]int{node}, path...)
+		}
+		return path
+	}
+
+	return []int{}
 }
--- algorithms/breadth_first_search_test.go
+++ algorithms/breadth_first_search_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestBreadthFirstSearch(t *testing.T) {
+func TestBreadthFirstSearchBool(t *testing.T) {
 
 	// start, end, test graph (end value exists in graph)
 
@@ -20,7 +20,7 @@ func TestBreadthFirstSearch(t *testing.T) {
 	test_start := 0
 	test_end := 13
 
-	res := BreadthFirstSearch(test_start, test_end, test_graph)
+	res := BreadthFirstSearchBool(test_start, test_end, test_graph)
 
 	AssertEquals(t, res, true)
 
@@ -29,7 +29,35 @@ func TestBreadthFirstSearch(t *testing.T) {
 	test_start = 0
 	test_end = 12
 
-	res = BreadthFirstSearch(test_start, test_end, test_graph)
+	res = BreadthFirstSearchBool(test_start, test_end, test_graph)
 
 	AssertEquals(t, res, false)
+}
+
+func TestBreadthFirstSearchPath(t *testing.T) {
+	// start, end, test graph (end value exists in graph)
+
+	test_graph := make(map[int][]int)
+
+	test_graph[0] = []int{1, 2, 3}
+	test_graph[2] = []int{4, 5, 6}
+	test_graph[3] = []int{7, 8}
+	test_graph[6] = []int{9, 10}
+	test_graph[10] = []int{13}
+
+	test_start := 0
+	test_end := 13
+
+	res := BreadthFirstSearchPath(test_start, test_end, test_graph)
+
+	AssertEquals(t, res, []int{0, 2, 6, 10, 13})
+
+	// start, end, test graph (end value not in graph)
+
+	test_start = 0
+	test_end = 12
+
+	res = BreadthFirstSearchPath(test_start, test_end, test_graph)
+
+	AssertEquals(t, res, []int{})
 }
